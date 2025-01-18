25297754fcf888a255cdb30b648fc5ca416df3a1
Hetharth Sachdeva <hetharthsach@gmail.com>
2025-01-18 12:42:51 +0530

    Updated LAnding Page

--- client/src/components/HeroCom/TopCho.jsx
+++ client/src/components/HeroCom/TopCho.jsx
@@ -20,15 +20,15 @@ const TopCho = () => {
   };
 
   return (
-    <div className='flex h-full flex-col items-center justify-center'>
-      <h1 className='md:text-9xl text-6xl font-bold text-blue-900 m-7 lora'>Top Suggested</h1>
+    <div className='flex h-full flex-col items-center justify-center bg-blue-500'>
+      <h1 className='md:text-7xl text-4xl font-bold text-white m-7 lora'>Top Suggested</h1>
       <div className='flex flex-col md:flex-row justify-center'>
         {filteredDoctors.map((doc, index) => {
           const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.1 });
           return (
             <motion.div
               key={doc.id}
-              className="flex items-center p-6 border-b bg-[#9999dc] flex-col m-4 rounded-3xl"
+              className="flex items-center p-6 border-b bg-[#1b58fe] flex-col m-4 rounded-3xl"
               custom={index}
               initial="hidden"
               animate={inView ? "visible" : "hidden"}
@@ -37,8 +37,8 @@ const TopCho = () => {
             >
               <img src={doctorImages[index]} alt={doc.name} className="rounded-full" />
               <div className="mt-4 text-center">
-                <h4 className="font-semibold text-lg">{doc.name}</h4>
-                <p className="text-sm text-gray-600">{doc.specialty}</p>
+                <h4 className="font-semibold text-lg text-[#fff]">{doc.name}</h4>
+                <p className="text-sm text-[#fff]">{doc.specialty}</p>
               </div>
             </motion.div>
           );
