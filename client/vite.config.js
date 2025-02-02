6b6b2d2b5e4e80e09276e624be2534c191e3067c
Prince Kumar <152782679+prince04kumar@users.noreply.github.com>
2025-02-02 19:21:52 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'https://health-record-app.onrender.com',
+      '/api':'https://health-record-app.onrender.com/',
     }
   },
   plugins: [react()],
