95b63b15f3d7b956189d08eaf906a3e9c8256914
Prince Kumar <152782679+prince04kumar@users.noreply.github.com>
2025-02-02 19:26:36 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':'https://health-record-app.onrender.com/',
-    }
-  },
   plugins: [react()],
   preview: {
     port: process.env.PORT || 3000,  // Bind to Render's port
